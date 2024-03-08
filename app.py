997477b6a1cf5c1821cd26eed4c7d72133a791e1
Stefan Grozdanovski <stefan.grozdanovski0@gmail.com>
2024-03-08 01:42:40 +0100

    Adding a button to the app

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, DevOps World!'
+def home():
+    # Render an HTML template for the homepage
+    return render_template('index.html')
 
+@app.route('/message')
+def message():
+    # This route will be triggered when the button is clicked
+    return 'You clicked the button!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
